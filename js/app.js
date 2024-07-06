0dd94ba81fd03afc48db63ccb614109e5f562d44
Davor Insaurralde <138895434+DavorTW@users.noreply.github.com>
2024-07-06 22:42:45 +0800

    Fixed issue with border that made the grid behave weirdly Added logic to draw the squares

--- js/app.js
+++ js/app.js
@@ -1,13 +1,25 @@
 const gridContainer = document.querySelector(".grid-container");
-let numberOfSquares = 32;
-const GRID_CONTAINER_WIDTH = 512
+let numberOfSquares = 64;
+const GRID_CONTAINER_WIDTH = 512;
 
 for (let i = 0; i < (numberOfSquares*numberOfSquares); i++) { 
     //here can also use a nested loop but i think
     //just multiplying the number by itself is a better approach
     const grid = document.createElement("div");
     grid.setAttribute("class", "grid");
-    grid.style.height = `${GRID_CONTAINER_WIDTH / numberOfSquares}px`;
-    grid.style.width = `${GRID_CONTAINER_WIDTH / numberOfSquares}px`;
+    grid.style.height = `${(GRID_CONTAINER_WIDTH / numberOfSquares)}px`;
+    grid.style.width = `${(GRID_CONTAINER_WIDTH / numberOfSquares)}px`;
     gridContainer.appendChild(grid);
 }
+
+const grids = document.querySelectorAll(".grid");
+let mouseIsDown = false;
+grids.forEach( grid => {
+    grid.addEventListener("mousedown", () =>{mouseIsDown=true});
+    grid.addEventListener("mouseup", () => {mouseIsDown=false});
+    grid.addEventListener("mousemove", ()=> {
+        if(mouseIsDown){
+            grid.style.backgroundColor = "black";
+        }
+    })
+})
